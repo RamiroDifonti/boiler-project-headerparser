7da58730af402293ad4e067f1fd7835aab3e49b2
Ramiro <alu0101425030@ull.edu.es>
2024-08-15 23:55:25 +0100

    added

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/whoami", function (req, res) {
+  console.log(req.headers["user-agent"])
+  res.json({ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers["user-agent"]});
+});
+
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
